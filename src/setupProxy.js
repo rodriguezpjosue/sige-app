0e1805db97b14d83dec8cc631cf319fa5a9d225d
Josue Rodriguez <rodriguezpjosue@gmail.com>
2022-06-10 07:01:28 -0500

    Build

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,9 @@ module.exports = function(app) {
   app.use(
     '/rest',
     createProxyMiddleware({
-      // target: 'https://sige.emmanuel.pe',
-      target: 'http://localhost:8069',
+      target: 'https://sige.emmanuel.pe',
+      // target: 'http://localhost:8069',
+      // target: 'http://localhost:7069',
       changeOrigin: true,
     })
   );
